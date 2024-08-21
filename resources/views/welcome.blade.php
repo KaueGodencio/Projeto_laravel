75ebd1f95cc75540f60fe0bc42a900c40a2461c5
Kaue Godencio <kauegodencio67@gmail.com>
2024-08-20 23:55:22 -0300

    realizei ajustes na views home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,32 +5,47 @@
 @section('content')
 
 
-<div id="search-container" class="col-md-12 d-flex justify-content-center">
- 
-        <h2 class="text-primary">Buscar evento</h2>
-   
 
-    <div class="row ">
-        <form action="/" method="GET" class="d-flex col-12 col-md-6">
+<div id="search-container" class="col-md-12 d-flex justify-content-center">    
+    <h2 class="text-primary">Buscar evento</h2>
+    <div class="row d-flex justify-content-center">
+        <form action="/" method="GET" class="d-flex col-12 col-md-6 col-lg-4">
             <input type="text" id="search" name="search" class="form-control mr-2 " placeholder="Procurar">
             <button type="submit" class="btn btn-primary">Pesquisar</button>
         </form>
 
+        @if ($search)
+            <div class="col-12 my-3">
+                <p class="mb-0 text-white ">Busca realizada por: <b>{{$search}}</b></p>
+            </div>
+        @endif
+        @if (count($events) == 0 && $search)
+            <div class="row d-flex justify-content-center ">
+                <div class="alert alert-danger col-12 col-md-8 " role="alert" id="alerta-danger">
+                    <p class="mb-0">Não encontramos nenhum evento correspondente a "<b>{{$search}}</b>." Tente usar outros
+                        termos de busca ou clique em . <a href="/">Ver Eventos</a></p>
+                </div>
+            </div>      
+        @endif
     </div>
 </div>
 
 <div class="container border">
     <div id="events-container" class="col-md-12 ">
         @if ($search)
-            
-            <div class="alert alert-primary mt-2 col-12" role="alert" id="alerta-danger">
-            <p class="mb-0">Busca realizada por: <b>{{$search}}</b></p>
-            </div>
+
+
         @else 
             <h1 class="mt-3 ">Proximos Evento</h1>
             <p>Veja os eventos dos proximos dias </p>
         @endif
-        
+
+        @if(count($events) == 0)
+            <div class="alert alert-primary col-12 my-3" role="alert">
+                <p class="mb-0">Não há eventos disponiveis. <span><a href="/events/create">Criar Eventos</a></span></p>
+            </div>
+            @endif
+
         <div id="card-container" class="row ">
             @foreach($events as $event)
                 <div class=" col-md-4 col-lg-3  ">
@@ -46,17 +61,10 @@
                     </div>
                 </div>
             @endforeach
-            @if (count($events) == 0 && $search)
-                <div class="alert alert-danger col-12" role="alert" id="alerta-danger">
-                    <p class="mb-0">Não encontramos nenhum evento correspondente a "<b>{{$search}}</b>." Tente usar outros termos de busca ou clique em . <a href="/">Ver Eventos</a></p>
-                </div>
-            @elseif(count($events)==0)
-            <div class="alert alert-primary col-12" role="alert">
-                    <p class="mb-0">Não há eventos disponiveis. <span><a href="/events/create">Criar Eventos</a></span></p>
-                </div>
 
 
-            @endif
+            
+
         </div>
     </div>
 </div>
