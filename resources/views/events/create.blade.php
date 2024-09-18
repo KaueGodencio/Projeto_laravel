a2cfef076c67f11b1e5dc931288fcbb6e1276a6f
Kaue <kauegodencio67@gmail.com>
2024-09-18 11:47:24 -0300

    realzei melhoraias no layout nas views de dashboard e na de criar eventos

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,85 +4,88 @@
 
 @section('content')
 
-<div id="event-create-container" class="col-md-6 offset-md-3  p-3">
-    <h1>Crie o seu evento</h1>
-
-    <form action="/events" method="POST" enctype="multipart/form-data" class="border my-3 p-3 rounded">
-        @csrf
-        <div class="form-group d-flex flex-column">
-            <label for="image"> Imagem do Evento:</label>
-            <input type="file" id="image" name="image" class="form-control-file">
-
-        </div>
-        
-        <div class="form-group">
-            <label for="title" class="fw-bold"> Evento:</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
-
-        </div>
-
-        <div class="form-group d-flex flex-column">
-            <label for="date"> Data do Evento:</label>        
-            <input type="date" name="date" id="date" class="form-control">
-            
-        </div>
-        <div class="form-group">
-            <label for="title"> Cidade:</label>
-            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
-
-        </div>
-        <div class="form-group">
-            <label for="title"> O evento é privado?</label>
-            <select name="private" id="private" class="form-control">
-                <option value="0">Não</option>
-                <option value="1">Sim</option>
-            </select>
-
-        </div>
-        <div class="form-group">
-            <label for="title">Descrição:</label>
-            <textarea name="description" id="description" class="form-control"
-                placeholder="O que vai acontecer no evento"></textarea>
-
-        </div>
-        <div class="form-group">
-            <label for="title">Adicione itens de infraestrutura:</label>
-
-            <div class="col-12">
+<div class="container border border-danger">
 
 
 
-                <div class="form-group">
-                    <input class="form-check-input" name="items[]" type="checkbox" value="Cadeiras">Cadeiras
-                </div>
-                <div class="form-group">
-                    <input class="form-check-input" name="items[]" type="checkbox" value="Mesas">Mesas
-                </div>
-                <div class="form-group">
-                    <input class="form-check-input" name="items[]" type="checkbox" value="Palco">Palco
-                </div>
-                <div class="form-group">
-                    <input class="form-check-input" name="items[]" type="checkbox" value="Cafe">Café
-                </div>
-                <div class="form-group">
-                    <input class="form-check-input" name="items[]" type="checkbox" value="Agua">água
-                </div>
-                <div class="form-group">
-                    <input class="form-check-input" name="items[]" type="checkbox" value="Brindes">Brindes
-                </div>
+    <div id="event-create-container" class="col-md-12   p-3">
+        <h1>Crie o seu evento</h1>
+
+        <form action="/events" method="POST" enctype="multipart/form-data" class="border my-3 p-3 rounded">
+            @csrf
+            <div class="form-group d-flex flex-column">
+                <label for="image"> Imagem do Evento:</label>
+                <input type="file" id="image" name="image" class="form-control-file">
             </div>
 
+            <div class="form-group">
+                <label for="title" class="fw-bold"> Evento:</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+            </div>
+
+            <div class="form-group d-flex flex-column">
+                <label for="date"> Data do Evento:</label>
+                <input type="date" name="date" id="date" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="title"> Cidade:</label>
+                <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+            </div>
+            
+            <div class="form-group">
+                <label for="title"> O evento é privado?</label>
+                <select name="private" id="private" class="form-control">
+                    <option value="0">Não</option>
+                    <option value="1">Sim</option>
+                </select>
+
+            </div>
+            <div class="form-group">
+                <label for="title">Descrição:</label>
+                <textarea name="description" id="description" class="form-control"
+                    placeholder="O que vai acontecer no evento"></textarea>
+
+            </div>
+            <div class="form-group">
+                <label for="title">Adicione itens de infraestrutura:</label>
+
+                <div class="col-12">
 
 
-        </div>
 
-        <input type="submit" class="btn btn-primary" value="Criar Evento">
+                    <div class="form-group">
+                        <input class="form-check-input" name="items[]" type="checkbox" value="Cadeiras">Cadeiras
+                    </div>
+                    <div class="form-group">
+                        <input class="form-check-input" name="items[]" type="checkbox" value="Mesas">Mesas
+                    </div>
+                    <div class="form-group">
+                        <input class="form-check-input" name="items[]" type="checkbox" value="Palco">Palco
+                    </div>
+                    <div class="form-group">
+                        <input class="form-check-input" name="items[]" type="checkbox" value="Cafe">Café
+                    </div>
+                    <div class="form-group">
+                        <input class="form-check-input" name="items[]" type="checkbox" value="Agua">água
+                    </div>
+                    <div class="form-group">
+                        <input class="form-check-input" name="items[]" type="checkbox" value="Brindes">Brindes
+                    </div>
+                </div>
+
+
+
+            </div>
+
+            <input type="submit" class="btn btn-primary" value="Criar Evento">
 
 
 
 
-    </form>
+        </form>
 
+    </div>
 </div>
 
 @endsection
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -2,10 +2,12 @@
 
 @section('title', $event->title)
 
-
-
 @section('content')
-<div class="col-md-10 offset-md-1">
+
+<div class="container">
+
+
+<div class="col-md-12 ">
     <div class="row">
         <div id="imag-container" class="col-md-6">
             <img src="/img/events/{{$event->image}}" class="img-fluid" alt="{{$event->title}}">
@@ -20,10 +22,13 @@
                     <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
                 </svg>{{$event->city}}
             <p class="events-participants"> X Participante</p>
-            <p class="event-owener"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person-check" viewBox="0 0 16 16">
-  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
-  <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z"/>
-</svg> {{$eventOwner['name']}}</p>
+            <p class="event-owener"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
+                    class="bi bi-person-check" viewBox="0 0 16 16">
+                    <path
+                        d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                    <path
+                        d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
+                </svg> {{$eventOwner['name']}}</p>
             <a href="" class="btn btn-primary" id="event-submit"> Confirmar Presença</a>
             <h4>O evento conta com:</h4>
             <div id="items">
@@ -45,7 +50,7 @@
 
             </div>
 
-           
+
         </div>
         <div class="col-md-12">
             <h3>Sobre o evento:</h3>
@@ -57,6 +62,7 @@
 
     </div>
 
+</div>
 </div>
 
 
